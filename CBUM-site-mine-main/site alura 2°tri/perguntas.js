0e3f2dbd67f7a2f06f6ae96cbdcdbe466214472d
Sussai <daniel.sussai.martins@escola.pr.gov.br>
2025-05-07 14:34:40 -0300

    Add files via upload

---
+++ CBUM-site-mine-main/site alura 2°tri/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Poções',
+    'Qual é o item necessário para criar uma "poção de cura" em Minecraft?',
+    'Para criar uma poção de cura, você precisa de uma garra de ghast e uma poção de cura instantânea (gerada a partir de uma poção de cura com água). A garra de ghast é o ingrediente que transforma a poção em "cura instantânea".'
+)
+
+criaCartao(
+    'Mobs',
+    'Qual é a chance de um "zumbi" ou "esqueleto" segurar um item em Minecraft?',
+    'A chance de um zumbi ou esqueleto segurar um item é de 5%. No entanto, isso pode variar dependendo do tipo de mob, como zumbis que podem carregar itens mais frequentemente, incluindo armaduras e ferramentas.'
+)
+
+criaCartao(
+    'Encantamentos',
+    'Qual é o número máximo de encantamentos diferentes que podem ser aplicados a um único item (como uma espada)?',
+    'O número máximo de encantamentos que podem ser aplicados a um item depende da combinação deles, mas, geralmente, você pode ter até 5 encantamentos diferentes em uma espada. Porém, encantamentos incompatíveis, como Afiação e Sofrimento, não podem ser aplicados juntos.'
+)
+
+criaCartao(
+    'Minecraft',
+    'Em qual ano o minecraft foi lancado e quem criou o minecraft?',
+    'O Minecraft foi lançado oficialmente em 2011, mas sua versão inicial (alpha) foi lançada em 2009. O jogo foi criado por Markus "Notch" Persson, um desenvolvedor sueco, que fundou a Mojang, a empresa responsável por Minecraft.'
+)
